c9158bb3d7f656eb43dff72a93aacc48f2eacbb3
Sai Charan SR <77528746+saicharan2008@users.noreply.github.com>
2021-08-04 20:57:08 +0530

    Add files via upload

---
+++ pro-c93-database/Config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+
+
+ const firebaseConfig = {
+  apiKey: "AIzaSyBmGcqq_ohzjubl9Y263vzAoSo9608JqtA",
+  authDomain: "database-6b054.firebaseapp.com",
+  projectId: "database-6b054",
+  storageBucket: "database-6b054.appspot.com",
+  messagingSenderId: "894834080619",
+  appId: "1:894834080619:web:7f4d97182b7510f9d32a2e"
+};
+
+  export default firebase.database;
